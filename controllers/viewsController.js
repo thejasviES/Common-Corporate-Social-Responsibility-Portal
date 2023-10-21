cb1cf82dc212f10ddaccc62273f8dfb351924aa3
MadakariNayakaHM <madakarinayakadinesh6282@gmail.com>
2023-10-21 18:50:30 +0530

    frontend updated

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,3 +14,22 @@ exports.signupPage = async (req, res, next) => {
 exports.loginPage = async (req, res, next) => {
     res.status(200).render('login');
 }
+exports.createEvent=async(req,res,next)=>
+{
+    res.status(200).render('eventForm')
+}
+exports.viewEvents= async (req, res, next) => {
+    const services=await Service.find()
+    const id= req.params.id
+    console.log(services)
+    var currentDate = new Date();
+    var year = currentDate.getFullYear();
+    var month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Add 1 to month because it's zero-based
+    var day = String(currentDate.getDate()).padStart(2, '0');
+    
+    var date = year + '-' + month + '-' + day;
+    // console.log(formattedDate);
+    
+    // console.log(id.services[0].createdBy._id)
+    res.status(200).render('viewEvents',{services,id, date});
+}
