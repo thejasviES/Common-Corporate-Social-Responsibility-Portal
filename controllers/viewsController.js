b25be7bb654eeb2d6c8f35868df7bc0411c945ee
MadakariNayakaHM <madakarinayakadinesh6282@gmail.com>
2023-10-21 16:47:21 +0530

    forgot what i did

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const User = require('./../models/userModel');
-
+const Service=require("./../models/serviceModel")
 
 exports.homePage = async (req, res, next) => {
-    res.status(200).render('home');
+    const users= await User.find();
+    res.status(200).render('home',{users});
 }
 
 exports.signupPage = async (req, res, next) => {
@@ -12,4 +13,12 @@ exports.signupPage = async (req, res, next) => {
 
 exports.loginPage = async (req, res, next) => {
     res.status(200).render('login');
+}
+
+exports.viewEvents= async (req,res,next)=>
+{   const services= await Service.find();
+    const id=req.params.id
+    console.log(id,services[0].createdBy._id),
+    
+    res.status(200).render('viewEvents',{services,id})
 }
--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -9,10 +9,11 @@ const authController = require('./../controllers/authController');
 
 Router.use(authController.isLoggedIn);
 
-Router.route('/eventRegister').get(viewsController.e)
+// Router.route('/eventRegister').get(viewsController.e)
 Router.route('/home').get(viewsController.homePage);
 Router.get("/signup", viewsController.signupPage);
 Router.route('/login').get(viewsController.loginPage);
+Router.route('/viewEvents/:id').get(viewsController.viewEvents);
 
 
 
